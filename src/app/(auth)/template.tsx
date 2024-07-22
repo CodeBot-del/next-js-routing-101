b8167f063238dc740fe3e2e11373d2f417a43fd4
CodeBot-del <stevenmanangu360@gmail.com>
2024-07-22 18:48:10 +0300

    templates

--- src/app/(auth)/template.tsx
+++ src/app/(auth)/template.tsx
@@ -3,6 +3,7 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";  //for checking active links using url paths
 import "./styles.css"
+import { useState } from "react";
 
 const navLinks = [
     {name: "Register", href: "/register"},
@@ -16,9 +17,13 @@ export default function AuthLayout({
     children: React.ReactNode;
 }) {
     const pathname = usePathname();
+    const [input, setInput] = useState("");
     
     return (
         <div>
+            <div>
+                <input value={input} onChange={e => setInput(e.target.value)}/>
+            </div>
             {
                 navLinks.map((link) => {
                     const isActive = pathname.startsWith(link.href)
