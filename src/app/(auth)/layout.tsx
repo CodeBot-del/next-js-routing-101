b74dd68f533d6a704f8e4005646f2ea97e5609f7
CodeBot-del <stevenmanangu360@gmail.com>
2024-07-22 18:37:43 +0300

    navigate programmatically

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { usePathname } from "next/navigation";  //for checking active links using url paths
 import "./styles.css"
 
 const navLinks = [
